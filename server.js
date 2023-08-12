f9635c0f1e2b0c5593f16d30d18b9a80b9e11f02
Arturo Morales <arturomorales@Arturos-MacBook-Pro.local>
2023-08-12 09:56:59 -0400

    Account page added and update function

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const port = 8080;
 const dbConn = require("./db");
 const path = require("path");
 const session = require("express-session");
+const multer = require('multer');
+const upload = multer({ dest: 'assets/uploaded_img/' }); // Set the appropriate destination for uploaded files
+
 
 // Set up the view engine and views directory
 app.set('view engine', 'ejs');
@@ -21,6 +24,8 @@ app.use(express.urlencoded({ extended: true })); // to read data from a post met
 // Serve static files from the "assets" directory
 app.use(express.static("assets"));
 
+let newUserId = 0;
+let currentUserId = 0;
 
 // Define routes
 app.get("/", (req, res) => {
@@ -34,30 +39,28 @@ app.post("/", (req, res) => {
   console.log('retrieved password: ' + loginPassword);
   // Query to verify if user exist in the database
   dbConn.query("SELECT * FROM User WHERE email = ?", loginEmail, function (err, results) {
-    if (results.length > 0) {
+    if (results) {
       console.log("Data of the user:");
       console.log(results);
-      if (results[0].password === loginPassword) {
+      if (results.length > 0 && results[0].password === loginPassword) {
         console.log("Password matches");
         const userData = results[0];
         // Set info of user in session
         req.session.user_data = userData;
-        // Redirect to dashboard
-        res.redirect("/dashboard");
+        currentUserId = results[0].user_id;
+        // Redirect to profile
+        res.redirect("/profile");
       } else {
-        console.log("Invalid Password");
+        console.log("Invalid Password or Email");
         res.render("index", { msg: "Invalid Email or Password" });
       }
     }
     else { 
       console.log(err);
-      console.log('Email not found');
-      res.render("index", { msg: "Invalid Email or Password" });
+      console.log('Failed database connection');
+      res.render("index", { msg: "Database connection failed" });
     }
-    
   });
-  
-
 });
 
 app.get("/register", (req, res) => {
@@ -83,7 +86,7 @@ app.post("/register", (req, res) => {
     'birthdate': birthdate,
     'gender': gender,
     'roommate_preference': preference
-  }
+  };
   // Query for adding new user into the database
   dbConn.query("INSERT INTO User SET ?", form_data, function (err, results) { 
     if (err) {
@@ -96,26 +99,35 @@ app.post("/register", (req, res) => {
       else { 
         res.render("register", { error: "Problem with user registration." });
       }
-      
     }
     else { 
       console.log(results);
+      newUserId = results.insertId;
       console.log('User added');
+      let newProfileData = {
+        'user_id': newUserId,
+        'profile_picture': '',
+        'additional_info': ''
+      };
+      dbConn.query("INSERT INTO Profile SET ?", newProfileData, function (err, results) { 
+        console.log(results);
+      });
       res.render("register", {success: 'Registration Successful!', error: '' });
     }
   });
   
 });
 
-app.get("/dashboard", function (req, res) { 
+app.get("/profile", function (req, res) { 
   if (!req.session.user_data) {
     console.log('User not logged in');
     res.redirect('/');
   }
   else { 
-    res.render("dashboard", {user: req.session.user_data});
+    dbConn.query("SELECT u.first_name, u.last_name, u.email, u.roommate_preference, p.additional_info FROM User u JOIN Profile p ON u.user_id = p.user_id WHERE u.user_id != ?", currentUserId, (err, rows) => { 
+      res.render("profile", { user: req.session.user_data, people: rows });
+    });
   }
-  
 });
 
 app.get("/logout", function (req, res) { 
@@ -123,6 +135,66 @@ app.get("/logout", function (req, res) {
   res.redirect('/');
 });
 
+app.get("/account", (req, res) => {
+  res.render("account", { user: req.session.user_data, error: "", success: ""});
+});
+
+app.post("/account", upload.single('picture'), (req, res) => {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  let password = req.body.password;
+  let birthdate = req.body.birthdate;
+  let gender = req.body.gender;
+  let preference = req.body.preference;
+  let picture = req.file ? req.file.buffer : null; // Use req.file.buffer if file was uploaded, or null if not
+  let info = req.body.info;
+  // Printing in console info of the uploaded picture
+  if (req.file) {
+    console.log("Uploaded file:", req.file);
+  }
+  console.log('updated User Data');
+  // Creating object with user's updated data
+  let updated_user_data = {
+    'first_name': firstName,
+    'last_name': lastName,
+    'email': email,
+    'password': password,
+    'birthdate': birthdate,
+    'gender': gender,
+    'roommate_preference': preference,
+  };
+  console.log(updated_user_data);
+  console.log("updated Profile Data");
+  let updated_profile_data = {
+    'additional_info': info
+  }
+  if (req.file) {
+    updated_profile_data["profile_picture"] = req.file.buffer;
+  }
+  console.log(updated_profile_data);
+  dbConn.query("UPDATE user SET ? WHERE user_id = ?" , [updated_user_data, currentUserId], function (err, userResults) { 
+    if (err) {
+      console.log("Error updating USER table:" + err);
+    } else { 
+      console.log(userResults);
+    }
+  });
+
+  dbConn.query("UPDATE profile SET ? WHERE user_id = ?", [updated_profile_data, currentUserId], function (err, profileResults) { 
+    if (err) {
+      console.log("Error updating PROFILE table: " + err);
+    } else {
+      console.log(profileResults);
+    }
+  });
+
+  res.render("account", {
+    user: req.session.user_data,
+    error: "",
+    success: "Data Updated",
+  });
+});
 
 // testing database simple query
 app.get("/all", (req, res) => {
@@ -134,7 +206,10 @@ app.get("/all", (req, res) => {
     }
   });
 });
-
+// Wrong URL, redirect to page not found
+app.use((req, res) => { 
+  res.render("404_Error");
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
